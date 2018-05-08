cede5f4a78adac13b6be815c3e87ff233efb21c8
kritikako <kritikakothari@gmail.com>
2018-05-08 16:37:36 -0500

    function to convert soil average depth into depth bounds

---
+++ tests/testthat/test_soilDepthAvg2Range.R
@@ -0,0 +1,25 @@
+#This is a test function that converts soil average depths into soil depth bounds.
+
+SoilDepthAvgToBound <- function(soilDepthAvg,topRef= 0){
+
+  #soilDepthAvg <- c(12,48,62,75,92,112,130,148)
+
+n <- length(soilDepthAvg)
+
+ans <- data.frame(soilDepthAvg,
+  layer_top=rep(NA, n),
+                  layer_bottom=rep(NA, n))
+
+ans$layer_top[1] <- 0 
+ans$layer_bottom[1] <- with(ans, (soilDepthAvg[1]-layer_top[1])*2 + layer_top[1])
+
+for(ii in 2:n){
+  ans$layer_top[ii] <- ans$layer_bottom[ii-1]
+  ans$layer_bottom[ii] <- with(ans, (soilDepthAvg[ii]-layer_top[ii])*2 + layer_top[ii])
+}
+
+return(ans)
+}
+
+soilDepthAvg <- c(12,48,62,75,92,112,130,148)
+x<- SoilDepthAvgToBound(soilDepthAvg,0)
