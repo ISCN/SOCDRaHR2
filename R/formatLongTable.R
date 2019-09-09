940341dc5dcd169011177f24aa812d0e70314e70
K Todd-Brown <ktoddbrown@gmail.com>
2019-09-09 14:33:44 -0400

    delete old commented out codefrom formatLongTable

--- R/formatLongTable.R
+++ R/formatLongTable.R
@@ -47,7 +47,7 @@ formatLongTable <- function(data.ls, sourceKey, targetKey,
   ##keep only keys that are well defined
   key <- key[!is.na(header) & !is.na(table_target) & !is.na(variable) & !is.na(type)]
   
- 
+  
   ##construct the resulting long table format with specified ID keys
   ans <- list()
   #for each target table, force an ordering
@@ -75,10 +75,6 @@ formatLongTable <- function(data.ls, sourceKey, targetKey,
       
       if(any(xx$type != 'id')){
         sourcedata <- melt(sourcedata, id.vars = idVars, variable.name = 'header', value.name='entry', na.rm=TRUE)
-        ##mutate factors
-        #profile_long.dt[,c('dataset_name_sub', 'site_name', 'profile_name') := lapply(.SD, as.factor), .SDcols=c('dataset_name_sub', 'site_name', 'profile_name')]
-        ##set keys
-        #data.table::setkeyv(profile_long.dt, c('dataset_name_sub', 'site_name', 'profile_name'))
         
         temp <- merge(sourcedata, key[type != 'id',c('variable', 'header', 'entry', 'type')], by='header')
         temp$entry <- paste0(temp$entry.x, temp$entry.y)
@@ -96,84 +92,8 @@ formatLongTable <- function(data.ls, sourceKey, targetKey,
       }else{
         ans[[targetTbl]] <- rbind(ans[[targetTbl]], sourcedata, fill=TRUE)
       }
-
-    #   
-    #   ##keep old names and join file later
-    #   ##rename headers => variable_type
-    #   names(sourcedata) <- paste0((xx[xx$header != '',])$variable, '_',
-    #                               (xx[xx$header != '',])$type)
-    #   #print(sourcedata)
-    #   
-    #   ##keep track of any basic patters (ie key$type) 
-    #   ##...that will need to be converted to long format at the end
-    #   meltPatternStr <- c()
-    #   
-    #   ##add any hard coded information from the key (generally units or methods)
-    #   hardEntry <- xx[xx$header == '',]
-    #   
-    #   if(nrow(hardEntry) > 0){
-    #     sourcedata <- cbind(sourcedata, 
-    #                         #set the hard entries
-    #                         as.data.frame(stats::setNames(as.list(hardEntry$entry),
-    #                                                       paste0(hardEntry$variable, '_', hardEntry$type))))
-    #     meltPatternStr <- unique(c(hardEntry$type), meltPatternStr)
-    #   }
-    #   
-    #   ##add any entries from the source data tables
-    #   softEntry <- xx[xx$header != '' & xx$type != 'id',]
-    #   if(nrow(softEntry) > 0){
-    #     sourcedata <- cbind(sourcedata, 
-    #                         #preserve the variable names
-    #                         as.data.frame(stats::setNames(as.list(unique(softEntry$variable)),
-    #                                                       paste0(unique(softEntry$variable), '_name'))))
-    #     meltPatternStr <- unique(c(softEntry$type, 'name', meltPatternStr))
-    #   }
-    #   
-    #   ##Transform data from wide format to long format
-    #   if(length(meltPatternStr) > 0){
-    #     #make sure every variable has all the types filled in so things melt properly
-    #     possibleHeaders <- expand.grid(unique(c(softEntry$variable, hardEntry$variable)), meltPatternStr)
-    #     possibleHeaders$header <- sprintf('%s_%s', possibleHeaders[,1], possibleHeaders[,2])
-    #     
-    #     if(any(!(possibleHeaders$header %in% names(sourcedata)))){
-    #       missingHeaders <- as.data.frame(stats::setNames(as.list(rep(NA, nrow(possibleHeaders))), possibleHeaders$header))
-    #       missingHeaders <- missingHeaders[!(names(missingHeaders) %in% names(sourcedata))]
-    #       sourcedata <- cbind(sourcedata, missingHeaders[,!(names(missingHeaders) %in% names(sourcedata))])
-    #     }
-    #     
-    #     if(verbose){print(paste('source table names are:',  paste0(sort(names(sourcedata)), collapse = ', ')))}
-    #     
-    #     if(verbose){
-    #       problematic.tbl <- table(names(sourcedata))
-    #       print('problematic headers:'); 
-    #       print(names(problematic.tbl[problematic.tbl > 1]))
-    #       }
-    #     
-    #     #make sure ordering is correct so the melt gets orders right
-    #     data.table::setcolorder(sourcedata, neworder = sort(names(sourcedata)))
-    #     
-    #     
-    #     #and use the data table fancy melt
-    #     if(verbose){
-    #     sourcedata <- data.table::melt.data.table(sourcedata,
-    #                        measure=patterns(paste0('_', meltPatternStr, '$')), 
-    #                        value.name = meltPatternStr)[,-'variable']
-    #     }else
-    #       #The NA's for the added columns read as boolean so melt throws a whole bunch of conversion warnings. Suppressing all of these right now and moving on
-    #       suppressWarnings({
-    #         sourcedata <- data.table::melt.data.table(sourcedata,
-    #                        measure=patterns(paste0('_', meltPatternStr, '$')), 
-    #                        value.name = meltPatternStr)[,-'variable']
-    #       })
-    #   }
-    #   
-    #   ##merge it with the data from other tables
-    #   if(nrow(ans[[targetTbl]]) == 0){
-    #     ans[[targetTbl]] <- sourcedata
-    #   }else{
-    #     ans[[targetTbl]] <- rbind(ans[[targetTbl]], sourcedata, fill=TRUE)
-    #   }
-     }
+      
+    }
   }
   
   return(ans)
