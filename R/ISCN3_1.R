48d6604f00abd659e422612c25b18f71081ca91d
Rita Hippe <hipper@ufl.edu>
2021-04-15 13:18:45 -0400

    Template function

---
+++ R/ISCN3_1.R
@@ -0,0 +1,23 @@
+ISCN3_1 <- function(){
+  ans <- data.frame()
+  
+  #read in the data
+  #set up standardCast()
+  #define dataset name from dataframe
+  #extract study info
+  #filter appropriate columns for that dataset
+  #only include columns that aren't entirely NA
+  #compiling citation and dataset columns
+  #running standardCast()
+  #taking profile and layer info
+  #if rows contain "ISCN" in dataset_name_soc, filling set columns (`soc_depth (cm)`, `soc (g cm-2)`, soc_carbon_flag, soc_spatial_flag, soc_method) with NA, otherwise leaving value as is
+  
+  
+  #put if statements to catch if it's a particular dataset/frame which will perform special functions to do what we need to
+  
+  
+  
+  
+  
+  return(ans)
+}
