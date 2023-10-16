45f8774899b99facd4aaa26609082d9649ccca34
Ursa Pillay <ursa.pillay@gmail.com>
2023-10-16 16:29:02 -0400

    finalize transformLong function

--- R/transformLong.R
+++ R/transformLong.R
@@ -1,34 +1,54 @@
-#' Convert dataset into long format
+#' Convert database into long format
 #' 
-#' @param data.df is original dataset
+#' @param data.ls is a list of tables in database
 #' @param annotations is data annotations from google sheet in dataframe
+#' 
+#' @import tidyverse
 #'
 #' @return dataframe with shoestringed data
 #' @export
 #' 
 
-transformLong <- function(data.df, annotations) {
+transformLong <- function(data.ls, annotations) {
+  # data.ls <- list(table1 = tibble(col1 = 1:3, col2 = c("A","B","C")), 
+  #                 table2 = tibble(col1 = 11:13, col2b = letters[1:3])
+  #                   )
+  # 
+  # annotations <- tribble(~table_id, ~column_id, ~of_type, ~with_entry,
+  #                        "table1", "col1", "unit", "count",
+  #                        "table1", "col1", "value", "--",
+  #                        "table1", "col2", "value", "--",
+  #                        "table2", "col1", "value", "--",
+  #                        "table2", "col2b", "value", "--",
+  #                        "table1", "col2", "description", "clevernamehere",)
   
-  #check if dataset is too large
-  
-  ans <- data.df %>%
-    mutate(study_id = as.character(annotations[1, "study_id"]), 
-           table_id = as.character(annotations[1, "table_id"]))  %>%
-    #Group by the columns that are identifiers
-    group_by(across(all_of((annotations %>%
-                              filter(is_type == 'identifier'))$column_id))) %>%
-    #Pull the current group index or id 
-    mutate(observation_id = cur_group_id()) %>%
-    #and then remove the grouping so we can move on
-    ungroup() %>%
-    pivot_longer(cols = -c(study_id, table_id, observation_id), names_to = 'column_id', 
-                 values_to = 'with_entry', values_drop_na = TRUE) %>%
+  ans <- plyr::ldply(.data = data.ls, .fun = function(x) {
+    if("row_number" %in% colnames(x)) {
+      warning("Replacing row_number with row order and using as a unique identifier.")
+    }
+    
+    #transform long
+    temp <- x %>%
+      mutate(across(.cols = everything(), .fns = as.character)) %>%
+      
+      ungroup() %>%
+      mutate(row_number = 1:n()) %>%
+      
+
+      ungroup() %>%
+      pivot_longer(cols = -c(row_number), names_to = 'column_id',
+                   values_to = 'with_entry', values_drop_na = TRUE)
+      
+      return(temp)
+    }, .id = "table_id") %>%
     full_join(annotations, 
-              by = join_by(study_id, table_id, column_id),
-              suffix = c('.data', ''), multiple = 'all') %>%
+            by = join_by(table_id, column_id),
+            suffix = c('.data', ''),
+            relationship = "many-to-many") %>%
     mutate(
       with_entry = if_else((with_entry == "--") | is.na(with_entry), with_entry.data, with_entry)) %>%
     select(-with_entry.data)
+  
   
   return(ans)
 }
