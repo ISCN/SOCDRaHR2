9bb081a5e95ad1b98493f9182ecb5590d4f5d80b
Ursa Pillay <ursa.pillay@gmail.com>
2023-10-16 17:17:16 -0400

    comment transformLong function

--- R/transformLong.R
+++ R/transformLong.R
@@ -10,7 +10,8 @@
 #' 
 
 transformLong <- function(data.ls, annotations) {
-  # data.ls <- list(table1 = tibble(col1 = 1:3, col2 = c("A","B","C")), 
+  # test data
+  # data.ls <- list(table1 = tibble(col1 = 1:3, col2 = c("A","B","C")),
   #                 table2 = tibble(col1 = 11:13, col2b = letters[1:3])
   #                   )
   # 
@@ -22,32 +23,43 @@ transformLong <- function(data.ls, annotations) {
   #                        "table2", "col2b", "value", "--",
   #                        "table1", "col2", "description", "clevernamehere",)
   
+  #pivot each dataframe longer and bind into one long table
   ans <- plyr::ldply(.data = data.ls, .fun = function(x) {
+    
+    #check if row_number column already exists
     if("row_number" %in% colnames(x)) {
       warning("Replacing row_number with row order and using as a unique identifier.")
     }
     
-    #transform long
     temp <- x %>%
+      
+      #convert all columns to character type
       mutate(across(.cols = everything(), .fns = as.character)) %>%
       
+      #give each row a unique number
       ungroup() %>%
       mutate(row_number = 1:n()) %>%
       
-
-      ungroup() %>%
+      #pivot longer
       pivot_longer(cols = -c(row_number), names_to = 'column_id',
                    values_to = 'with_entry', values_drop_na = TRUE)
       
       return(temp)
     }, .id = "table_id") %>%
+    
+    #join long table with annotations
     full_join(annotations, 
             by = join_by(table_id, column_id),
             suffix = c('.data', ''),
             relationship = "many-to-many") %>%
+    
+    #replace value placeholders in with_entry column with values
     mutate(
       with_entry = if_else((with_entry == "--") | is.na(with_entry), with_entry.data, with_entry)) %>%
-    select(-with_entry.data)
+    select(-with_entry.data) %>%
+    
+    #remove rows with no row number
+    drop_na(row_number)
   
   
   return(ans)
